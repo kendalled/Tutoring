8cc3d91a104852cadbdd12cfaf772fc4b1c491f2
Kendall Jackson <quailpecker@gmail.com>
2018-12-07 01:49:10 -0500

    Added interview traffic light example in py3
    v1 lol

---
+++ interview.py
@@ -0,0 +1,20 @@
+# The code that got me my first CS Job (Salaried at least)
+# Kendall Jackson, Winter Park FL, 12/04/2018
+def traffic_light(color):
+    x = color
+
+    # If statements handling all 3 possible cases, changes from G -> Y -> R
+    if(x == "green"):
+        x = "yellow"
+        print("Light changed to: {}\n--------------".format(x))
+
+    if(x == "yellow"):
+        x = "red"
+        print("Light changed to: {}\n--------------".format(x))
+
+    else:
+        x = "green"
+        print("Light changed to: {}\n--------------".format(x))
+
+# Must be lowercase
+traffic_light(input("Please type 'green', 'red', or 'yellow' to see the traffic light cycle through: \n"))
